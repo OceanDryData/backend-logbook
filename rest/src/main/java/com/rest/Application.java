533f23e5052d0cd67ea91086881bf3ab88e735d9
ReneRoets <rene.roets@outlook.com>
2019-05-01 15:49:22 +0200

    Fix cors policy

--- rest/src/main/java/com/rest/Application.java
+++ rest/src/main/java/com/rest/Application.java
@@ -3,16 +3,32 @@ package com.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 @EntityScan(basePackages = {"com"})
 @ComponentScan(basePackages = {"com"})
 @EnableJpaRepositories(basePackages = {"com"})
 public class Application {
+
     public static void main(String[] args){
         SpringApplication.run(Application.class);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
+
